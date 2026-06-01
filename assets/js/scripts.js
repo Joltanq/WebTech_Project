ed9cd710141840d7a0b59db7dc13fcf5342d05fd
joelt <joel.tan.92@gmail.com>
2026-06-02 02:45:37 +0800

    feat:able to close mobile nav

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,13 @@
 const hamburger = document.getElementById("mobile-menu-button");
 const mobileMenu = document.getElementById("mobile-nav-list");
+const closeButton = document.getElementById("close-mobile-nav-button");
 
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.remove("hidden");
     mobileMenu.classList.add("flex");
 })
 
-console.log("hamburger:", hamburger);
-console.log("mobileMenu:", mobileMenu);
+closeButton.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+    mobileMenu.classList.remove("flex");
+})
