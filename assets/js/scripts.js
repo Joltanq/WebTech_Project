2fb1d514a192c902640594f117d805794dd1c61a
joelt <joel.tan.92@gmail.com>
2026-06-07 02:14:02 +0800

    feat:add the ability to call API for babynames

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,32 +30,27 @@ async function callAPI(baseURL){
     return  response.json();
 }
 
-// submit button logic. calls the API when clicked and returns results. also handles errors
-document.getElementById("api_form").addEventListener("submit",async function(e) {
-    e.preventDefault();
-    const input = document.getElementById("api_parameter").value;
-    const dropdownValue = document.getElementById("baby_names_dropdown").value;
-    const resultsArea = document.getElementById("api_results");
-    const selectedApi = document.querySelector('input[name="api_picker"]:checked');
+function getGenderValue(){
+    // try to get from the card value first. if fail, check the dropdown value
+    const checkedRadio = document.querySelector('input[name="gender"]:checked');
+    if (checkedRadio) {
+        return checkedRadio.value;
+    } else {
+        return document.querySelector('select[name="gender"]').value;
+    }
+}
 
-    if (selectedApi.value === "baby_names"){
+// submit button logic. calls the API when clicked and returns results. also handles errors
+document.getElementById("baby_names_form").addEventListener("submit",async function(e) {
+    e.preventDefault();
+    const dropdownValue = getGenderValue();
+    const resultsArea = document.getElementById("api_results");
+
         try{
             const response_data = await callAPI(`https://api.api-ninjas.com/v1/babynames?gender=${dropdownValue}`);
             resultsArea.textContent = response_data[0];
         }catch(e){
             resultsArea.textContent = e;
-        }
-    }else if (selectedApi.value === "emoji" && (input === "smileys_emotion" || input === "people_body" ||input === "animals_nature" )){
-        try{
-            const response_data = await callAPI(`https://api.api-ninjas.com/v1/emoji?group=${input}`);
-            const emoji_obj = response_data[0];
-
-            resultsArea.textContent = emoji_obj.character;
-        }catch(e){
-            resultsArea.textContent = e;
-        }
-    }else{
-        resultsArea.style.color = '#bd1414';
-        resultsArea.textContent = "That's not right. Check that it is either smileys_emotion, people_body or animals_nature ";
     }
+
 })
